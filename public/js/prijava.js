26e18710506999d76ef5ea056b42e6eef8474b84
MihajloESTS <milojevicm374@gmail.com>
2022-02-02 22:26:08 +0100

    Prijava - Validacija na klijntovoj strani

--- public/js/prijava.js
+++ public/js/prijava.js
@@ -6,6 +6,9 @@ const emailDOM = document.getElementById("email");
 const prijavaDOM = document.getElementById("prijava");
 const porukaDOM = document.getElementById("poruka");
 
+const upsehColor = "lightgreen";
+const errorColor = "var(--fg)";
+
 async function Prijava()
 {
 	try {
@@ -25,13 +28,13 @@ async function Prijava()
 		})
 		const data = await response.json();
 		if(data.ok) {
-			Poruka("Uspešna prijava", "green");
+			Poruka("Uspešna prijava", upsehColor);
 		}
 		else throw new Error(data.message);
 	} 
 	catch (error) {
 		console.error(error);
-		Poruka(error.message, "var(--fg)");
+		Poruka(error.message, errorColor);
 	}
 	finally {
 		prijavaDOM.disabled = false;
@@ -53,15 +56,34 @@ function Submit()
 
 function Obavezno(e) {
 	const element = e.currentTarget;
-	console.log(element.value);
+	console.log(element.value.trim());
 	if(element.value)
-		element.style.borderColor = "green";
+		element.style.borderColor = upsehColor;
 	else
-		element.style.borderColor = "var(--fg)";
+		element.style.borderColor = errorColor;
 }
 
-imeDOM.addEventListener("input", Obavezno)
-jmbgDOM.addEventListener("input", Obavezno)
+imeDOM.addEventListener("input", (e) => {
+	imeDOM.value = imeDOM.value.trim()
+	Obavezno(e);
+	if((/[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~1-9]/).test(imeDOM.value))
+		imeDOM.style.borderColor = errorColor;
+})
+jmbgDOM.addEventListener("input", e => {
+	jmbgDOM.value = jmbgDOM.value.trim()
+	Obavezno(e);
+	if(jmbgDOM.value.length != 13 || !(/^\d+$/).test(jmbgDOM.value))
+		jmbgDOM.style.borderColor = errorColor;
+})
 adresaDOM.addEventListener("input", Obavezno)
-telefonDOM.addEventListener("input", Obavezno)
-emailDOM.addEventListener("input", Obavezno)
+telefonDOM.addEventListener("input", e => {
+	telefonDOM.value = telefonDOM.value.trim()
+	Obavezno(e);
+	if(!(/^\d+$/).test(telefonDOM.value))
+		telefonDOM.style.borderColor = errorColor;
+})
+emailDOM.addEventListener("input", e => {
+	Obavezno(e);
+	if(!(/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i).test(emailDOM.value))
+		emailDOM.style.borderColor = errorColor;
+})
